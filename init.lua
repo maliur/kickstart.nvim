e7644e7908ae898f5fb8b68bd28660e21ad8d5a0
Fredrik Anbacken <fredrikanbacken@tutanota.com>
2024-05-17 11:46:35 +0200

    Use tab and enter for autocomplete

--- init.lua
+++ init.lua
@@ -779,17 +779,18 @@ require('lazy').setup({
           --  completions whenever it has completion options available.
           ['<C-Space>'] = cmp.mapping.complete {},
 
-          ['<CR>'] = cmp.mapping {
-            i = function(fallback)
-              if cmp.visible() and cmp.get_active_entry() then
-                cmp.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false }
-              else
-                fallback()
+          ['<CR>'] = cmp.mapping(function(fallback)
+            -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+            if cmp.visible() then
+              local entry = cmp.get_selected_entry()
+              if not entry then
+                cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
               end
-            end,
-            s = cmp.mapping.confirm { select = true },
-            c = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
-          },
+              cmp.confirm()
+            else
+              fallback()
+            end
+          end, { 'i', 's', 'c' }),
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
