3ddaa4e3eab4f8601a4c0866e36686e15f0c970e
Fredrik Anbacken <fredrikanbacken@tutanota.com>
2024-12-13 21:47:45 +0100

    Update conform and add oil

--- init.lua
+++ init.lua
@@ -705,24 +705,44 @@ require('lazy').setup({
 
   { -- Autoformat
     'stevearc/conform.nvim',
-    opts = {
-      async = true,
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
       },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = 'never'
+        else
+          lsp_format_opt = 'fallback'
+        end
+        return {
+          timeout_ms = 500,
+          lsp_format = lsp_format_opt,
+        }
+      end,
       formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        javascript = { { 'prettierd' } },
-        typescript = { { 'prettierd' } },
-        typescriptreact = { { 'prettierd' } },
-        go = { { 'gofmt' } },
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        lua = { 'stylua', 'stylua', stop_after_first = true },
+        javascript = { 'prettierd', 'prettierd', stop_after_first = true },
+        typescript = { 'prettierd', 'prettierd', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettierd', stop_after_first = true },
+        go = { 'gofmt', 'gofmt', stop_after_first = true },
       },
     },
   },
@@ -896,6 +916,25 @@ require('lazy').setup({
       vim.cmd.colorscheme 'gruvbox'
       vim.cmd.hi 'Comment gui=none'
     end,
+  },
+
+  {
+    'stevearc/oil.nvim',
+    config = function()
+      vim.keymap.set('n', '<leader>-', require('oil').open_float, { desc = 'Oil: toggle float' })
+
+      require('oil').setup {
+        view_options = {
+          show_hidden = true,
+        },
+        float = {
+          padding = 8,
+        },
+      }
+    end,
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
 
   -- Highlight todo, notes, etc in comments
